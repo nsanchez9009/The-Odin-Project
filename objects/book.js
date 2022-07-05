4d772cc7c0d1b3d6c19e5de4e528739e0dfeebcd
Nicolas Sanchez <nsanchez9009@gmail.com>
2022-07-05 10:48:18 -0400

    OOP practice.

---
+++ objects/book.js
@@ -0,0 +1,28 @@
+function book(title, author, pages, read){
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    
+    this.info = () => {
+        let temp = `${title} by ${author}, ${pages} pages, `;
+
+        if (read.toLowerCase() === "no"){
+            temp += "not read yet";
+        }
+        else if (read.toLowerCase() === "yes"){
+            temp += "read";
+        }
+
+        return temp;
+    }
+}
+
+const title = prompt("Title: ");
+const author = prompt("Author: ");
+const pages = prompt("Number of pages: ");
+const read = prompt("Have you read this book? ");
+
+const theHobbit = new book(title, author, pages, read);
+
+console.log(theHobbit.info());
